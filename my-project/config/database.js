47f21816902044ec270c50eb72333d278e42e026
Ankit Patidar <ankit.patidar@ankitss292.successivetech.com>
2023-05-25 17:37:55 +0530

    remove

--- my-project/config/database.js
+++ my-project/config/database.js
@@ -19,19 +19,6 @@ module.exports = ({ env }) => {
         ssl: env.bool('DATABASE_SSL', true),
       },
     },
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: env('DATABASE_URI'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-    },
 
     mysql: {
       connection: {
